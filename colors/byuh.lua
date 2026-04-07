f835cc9f97e35a3aca53a97ecef14d6e0b27b987
-name <alui@lanl.gov>
2026-04-06 18:09:42 -0600

    improve red

--- colors/byuh.lua
+++ colors/byuh.lua
@@ -2,8 +2,7 @@ local color_scheme = {
   primary_bg    = "#161A1D", -- carbon
   cursor_line   = "#3B3B3B", -- darkgrey
 
-  -- literal       = "#FF6961", -- patel red
-  literal       = "#ba0c2f", -- red
+  literal       = "#DD3D5D", -- red
   function_name = "#c69214", -- gold
 
   comment       = "#696969", -- grey
