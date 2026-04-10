2b6482eac74e5231fed620890bfcf4dc3db20ccb
-name <alui@lanl.gov>
2026-04-10 16:40:20 -0600

    add red

---
+++ colors/red.lua
@@ -0,0 +1,16 @@
+-- Gradient based
+local color_scheme = {
+  -- Colors are listed from darkest to lightest
+
+  primary_bg    = "#161A1D", -- carbon
+
+  cursor_line   = "#410205",
+  comment       = "#9F3136",
+  literal       = "#AF5E62",
+  function_name = "#BF8B8D",
+  primary_fg    = "#CFB8B8",
+
+  special       = "#FFF5EE", -- smoke
+}
+
+require("theme").run("red", color_scheme)
